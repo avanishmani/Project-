a81e8662c342383946df1a052a39778425bc6905
avanishmani <avanishmanitripathi@gmail.com>
2023-04-03 00:43:31 +0530

    Day33

---
+++ SpringXml/Day33/src/main/java/com/mr/tripathi/Day33Application.java
@@ -0,0 +1,13 @@
+package com.mr.tripathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day33Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day33Application.class, args);
+	}
+
+}
