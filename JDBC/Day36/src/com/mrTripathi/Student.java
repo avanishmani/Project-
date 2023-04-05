f8bf08486c2e94e80e8330639e29eb858c8fae92
avanishmani <avanishmanitripathi@gmail.com>
2023-04-05 23:27:24 +0530

    Day36

---
+++ JDBC/Day36/src/com/mrTripathi/Student.java
@@ -0,0 +1,21 @@
+package com.mrTripathi;
+
+public class Student implements Comparable<Student>{  
+	int rollno;  
+	String name;  
+	int age;  
+	Student(int rollno,String name,int age){  
+	this.rollno=rollno;  
+	this.name=name;  
+	this.age=age;  
+	}  
+	public int compareTo(Student st){  
+	if(age==st.age)  
+	return 0;  
+	else if(age>st.age)  
+	return 1;  
+	else  
+	return -1;  
+	}  
+
+}
---
+++ JDBC/Day36/src/com/mrTripathi/Test.java
@@ -0,0 +1,22 @@
+package com.mrTripathi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<Student> al=new ArrayList<Student>();  
+		al.add(new Student(101,"Vijay",23));  
+		al.add(new Student(106,"Ajay",27));  
+		al.add(new Student(105,"Jai",21));  
+		  
+		Collections.sort(al);  
+		for(Student st:al){  
+		System.out.println(st.rollno+" "+st.name+" "+st.age);  
+		}  
+		}  
+
+	}
+
+
