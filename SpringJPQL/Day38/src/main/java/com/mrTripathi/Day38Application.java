e21fe1009f4b5eb75e3255a264fa9cb5067eb5ee
avanishmani <avanishmanitripathi@gmail.com>
2023-04-07 16:06:28 +0530

    Day 38

---
+++ SpringJPQL/Day38/src/main/java/com/mrTripathi/Day38Application.java
@@ -0,0 +1,13 @@
+package com.mrTripathi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day38Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day38Application.class, args);
+	}
+
+}
