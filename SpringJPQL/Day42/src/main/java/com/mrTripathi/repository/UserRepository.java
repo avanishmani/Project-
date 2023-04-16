4cb6626094040b84d4cb6bd528b522edf4ceba6c
avanishmani <avanishmanitripathi@gmail.com>
2023-04-16 23:49:01 +0530

    Day-1

--- SpringJPQL/Day42/src/main/java/com/mrTripathi/repository/UserRepository.java
+++ SpringJPQL/Day42/src/main/java/com/mrTripathi/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.mrTripathi.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mrTripathi.model.Users;
+@Repository
+public interface UserRepository extends JpaRepository<Users, String> {
 
 }
