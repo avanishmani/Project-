0138d2451c89ee1d65c5994fd7651a0a6838d17f
avanishmani <avanishmanitripathi@gmail.com>
2023-05-12 23:40:42 +0530

    12 May

---
+++ Recursion/src/com/Recursion/LeaderBoard.java
@@ -0,0 +1,47 @@
+package com.Recursion;
+
+import java.util.*;
+
+class Student implements Comparable<Student> {
+    String name;
+    int marks;
+
+    public Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if (this.marks == o.marks) {
+            return this.name.compareTo(o.name);
+        }
+        return o.marks - this.marks;
+    }
+}
+
+public class LeaderBoard {
+    public static void main(String[] args) {
+        Map<String, Integer> studentMap = new HashMap<>();
+        studentMap.put("John", 50);
+        studentMap.put("Jane", 60);
+        studentMap.put("Bob", 50);
+        studentMap.put("Alice", 70);
+        studentMap.put("Eve", 60);
+
+        List<Student> students = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : studentMap.entrySet()) {
+            students.add(new Student(entry.getKey(), entry.getValue()));
+        }
+        Collections.sort(students);
+
+        int rank = 1;
+        for (int i = 0; i < students.size(); i++) {
+            if (i > 0 && students.get(i).marks != students.get(i - 1).marks) {
+                rank = i + 1;
+            }
+            System.out.println(students.get(i).name + " " + rank);
+        }
+    }
+}
+
